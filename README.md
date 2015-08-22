fb20774561b7e03f9ee9b606c7d648ccd312aac5
Earl Hinrichs <earl@spyke.com>
2015-08-22 11:30:31 -0600

    Added more comments and tests to the script

--- README.md
+++ README.md
@@ -8,20 +8,25 @@ https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Datas
 
 The original data should be downloaded and unzipped before running the script.
 The location of the original data is put into the script variable projectFolder, if you
-use a different location, modify the variable for your location. 
+use a different location, modify the variable for your location. You can use "." for
+your working directory
 
-In my case, the file is unzipped into C:\Coursera\GetAndClean\Project.Wk3\
+In my case, the file is unzipped into C:\\Coursera\\GetAndClean\\Project.Wk3\\
 
-First the original data is read into memory, unmodified. 
+First, the original data is read into memory, unmodified. File paths are built up
+with the file.path command. The script is designed for Windows OS. If you are
+using Linux you will need to change the path separator.
 
-R Object | Original File
--------- | -------------
+
+R Object      | Original File
+------------- | -------------
 features      | features.txt
 activities    | activity_labels.txt
-subjectTrain  | train\subject_train.txt
-activityTrain | train\y_train.txt
-valuesTrain   | train\X_train.txt
-subjectTest   | test\subject_test.txt
-activityTest  | test\y_test.txt
-valuesTest    | test\X_test.txt
+subjectTrain  | train\\subject_train.txt
+activityTrain | train\\y_train.txt
+valuesTrain   | train\\X_train.txt
+subjectTest   | test\\subject_test.txt
+activityTest  | test\\y_test.txt
+valuesTest    | test\\X_test.txt
+
 
--- run_analysis.R
+++ run_analysis.R
@@ -1,74 +1,79 @@
-if(FALSE) { #skip these steps for now
+
+    #Location of original data.
 projectFolder <- "C:\\Coursera\\GetAndClean\\Project.Wk3"
 rawFiles      <- file.path(projectFolder,"UCI HAR Dataset",fsep="\\")
-    #print(list.files(rawFiles))
+    cat("Raw data files at:", rawFiles, "\n")
+
 features.txt  <- file.path(rawFiles,"features.txt",fsep="\\")
 features      <- read.table(features.txt)
-    #print(head(features))
+    cat("dim(features):", dim(features), "\n")
+
 activities.txt<- file.path(rawFiles,"activity_labels.txt",fsep="\\")
 activities    <- read.table(activities.txt)
-    print(head(activities))
+    cat("dim(activities):", dim(activities), "\n")
+    
+    #The various *train files should have the same number of rows.
 subjectTrain.txt <- file.path(rawFiles,"train","subject_train.txt",fsep="\\")
 subjectTrain     <- read.table(subjectTrain.txt)
-    print(head(subjectTrain))
-    print(dim(subjectTrain))
+    cat("dim(subjectTrain):", dim(subjectTrain), "\n")
 activityTrain.txt <- file.path(rawFiles,"train","y_train.txt",fsep="\\")
 activityTrain     <- read.table(activityTrain.txt)
-    print("head(activityTrain)")
-    print(head(activityTrain))
-    print("dim(activityTrain)")
-    print(dim(activityTrain))
+    cat("dim(activityTrain):", dim(activityTrain), "\n")
+    stopifnot( nrow(subjectTrain) == nrow(activityTrain) )
 valuesTrain.txt <- file.path(rawFiles,"train","X_train.txt",fsep="\\")
 valuesTrain     <- read.table(valuesTrain.txt)
-    print("head(valuesTrain)")
-    print(head(valuesTrain))
-    print("dim(valuesTrain)")
-    print(dim(valuesTrain))
+    cat("dim(valuesTrain):", dim(valuesTrain), "\n")
+    stopifnot( nrow(valuesTrain) == nrow(activityTrain) )
+    stopifnot( ncol(valuesTrain) == nrow(features) )
+    
+    #The various *text files should have the same number of rows.
 subjectTest.txt <- file.path(rawFiles,"test","subject_test.txt",fsep="\\")
 subjectTest     <- read.table(subjectTest.txt)
-    print("head(subjectTest)")
-    print(head(subjectTest))
-    print("dim(subjectTest)")
-    print(dim(subjectTest))
+    cat("dim(subjectTest):", dim(subjectTest), "\n")
+    
 activityTest.txt <- file.path(rawFiles,"test","y_test.txt",fsep="\\")
 activityTest     <- read.table(activityTest.txt)
-    print("head(activityTest)")
-    print(head(activityTest))
-    print("dim(activityTest)")
-    print(dim(activityTest))
+    cat("dim(activityTest):", dim(activityTest), "\n")
+    stopifnot( nrow(subjectTest) == nrow(activityTest) )
+    
 valuesTest.txt <- file.path(rawFiles,"test","X_test.txt",fsep="\\")
 valuesTest     <- read.table(valuesTest.txt)
-    print("valuesTest[1:5,1:5]")
-    print(valuesTest[1:5,1:5])
-    print("dim(valuesTest)")
-    print(dim(valuesTest))
-        #Assign user-friendly names to columns
-names(valuesTest)<-features[,2]
-names(valuesTrain)<-features[,2]
-    print("valuesTest[1:5,1:5]")
-    print(valuesTest[1:5,1:5])
-    print("valuesTrain[1:5,1:5]")
-    print(valuesTrain[1:5,1:5])
-        #Use rbind to combine test and train data sets
+    cat("dim(activityTest):", dim(activityTest), "\n")
+    stopifnot( nrow(valuesTest) == nrow(activityTest) )
+    stopifnot( ncol(valuesTest) == nrow(features) )
+
+
+    #Use rbind to combine test and train data sets
 values=rbind(valuesTest,valuesTrain)
-    print("dim(values)")
-    print(dim(values))
 subject=rbind(subjectTest,subjectTrain)
-    print("dim(subject)")
-    print(dim(subject))
-    stopifnot(subject<=30)
-    stopifnot(dim(subject)[2]!=1)
-names(subject)="subject"
 activity=rbind(activityTest,activityTrain)
-    print("dim(activity)")
-    print(dim(activity))
-    stopifnot(activity<=6)
-    stopifnot(dim(activity)[2]!=2)
-valuesOfInterest<-values[,grepl("mean\\(\\)|std\\(\\)|meanFreq\\(\\)",features[,2])]
+
+    #This is as good of time as any to clean up column names
+goodNames <- features[,2]
+goodNames <- gsub("\\(\\)","", goodNames)   #Remove ()
+goodNames <- gsub("-",".", goodNames)       #Change - to .
+names(values) <- goodNames
+    cat( "Varible name samples:", head(goodNames,4), "\n")
+names(subject)="subject"
+
+    #activity has activity numbers, names would be better.
+    #activities is a lookup table to translate between ids and names
+    #merge the two table on id, then drop everything but the name column
 activityName<-merge(activity,activities,by.x=1,by.y=1)[,2]
+    stopifnot( nrow(activityName) == nrow(activity) )
+names(activityName) = "activity"
+    
+    #We need only the mean and std values. These are identified by
+    #one of "mean()", "std()", meanFreq()" in the (original) variable name.
+    #subset the interesting columns
+valuesOfInterest<-values[,grepl("mean\\(\\)|std\\(\\)|meanFreq\\(\\)",features[,2])]
+    cat("dim(valuesOfInterest):", dim(valuesOfInterest), "\n")
+
+    #Finally! Nice names, number of rows match. Time for a cbind.
+    #The resulting table is written to the working directory
+    #Read back with read.table("tidy.txt",header=TRUE)
 tidy<-cbind(subject,activityName,valuesOfInterest)
-}
+    cat("dim(tidy):", dim(tidy), "\n")
 tidy.txt <- "tidy.txt"
 write.table(tidy, tidy.txt, row.name=FALSE)
-#Ready with read.table("tidy.txt",header=TRUE)
 
